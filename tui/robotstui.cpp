4902a8cdba44debfc67036806a3d13e663a38025
Seth H. Markarian <markaris@aec515ld-03.cs.lafayette.edu>
2019-02-21 13:29:54 -0500

    Merging

--- tui/robotstui.cpp
+++ tui/robotstui.cpp
@@ -4,3 +4,66 @@ RobotsTUI::RobotsTUI()
 {
 
 }
+
+void RobotsTUI::draw_screen() {
+
+}
+
+void RobotsTUI::run() {
+    // initialize the screen
+    initscr();
+
+    // hide the cursor from view (comment this line out for debugging)
+    curs_set(0);
+
+    // disables line buffering and erase/kill character-processing
+    // (interrupt and flow control characters are unaffected),
+    // making characters typed by the  user  immediately
+    // available to the program
+    cbreak();
+
+    // control whether characters  typed  by  the user  are echoed
+    // by getch as they are typed
+    noecho();
+
+    // the user can press a function key (such as an arrow key) and
+    // getch returns a single value representing the  function  key,
+    // as in KEY_LEFT
+    keypad(stdscr, TRUE);
+
+    // initialize the interaction loop to run
+    bool continue_looping = true;
+
+    // draw the current screen
+    draw_screen();
+
+    do {
+
+        // draw the new screen
+        refresh();
+
+        // obtain character from keyboard
+        int ch = getch();
+
+        // operate based on input character
+        switch (ch) {
+        case KEY_DOWN:
+            break;
+        case KEY_UP:
+            break;
+        case KEY_LEFT:
+            break;
+        case KEY_RIGHT:
+            continue_looping = false;
+            break;
+
+        }
+    } while(continue_looping);
+
+        // cleanup the window and return control to bash
+        endwin();
+
+        std::cout << "exiting run\n";
+
+}
+}
--- tui/robotstui.h
+++ tui/robotstui.h
@@ -1,11 +1,17 @@
 #ifndef ROBOTSTUI_H
 #define ROBOTSTUI_H
 
+#include "screen.h"
+#include
 
-class RobotsTUI
+
+class RobotsTUI : public Screen
 {
+protected:
+    void draw_screen();
 public:
     RobotsTUI();
+    void run();
 };
 
 #endif // ROBOTSTUI_H
